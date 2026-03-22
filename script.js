32597be5c9d166b93f3cebf718916bcbc7d088fa
JulianHarms <jperegrine911@gmail.com>
2026-03-22 16:34:18 -0700

    feat: Migrate signup form functionality from Flodesk to MailerLite, updating scripts, styles, and HTML.

--- script.js
+++ script.js
@@ -13,33 +13,3 @@ const revealObserver = new IntersectionObserver(
 );
 
 revealItems.forEach((item) => revealObserver.observe(item));
-
-const flodeskTriggers = document.querySelectorAll(".prequel-trigger");
-
-const openFlodeskPopup = () => {
-  let opened = false;
-
-  if (typeof window.fd === "function") {
-    window.fd("form", { formId: "69ac960345e6c74c868c4135" });
-    opened = true;
-  }
-
-  // Re-open an existing Flodesk modal instance if it is already in the DOM.
-  const modal = document.querySelector(".fd-modal");
-  if (modal) {
-    modal.classList.add("fd-is-open");
-    modal.setAttribute("aria-hidden", "false");
-    opened = true;
-  }
-
-  if (!opened) {
-    window.location.hash = "signup";
-  }
-};
-
-flodeskTriggers.forEach((trigger) => {
-  trigger.addEventListener("click", (event) => {
-    event.preventDefault();
-    openFlodeskPopup();
-  });
-});
